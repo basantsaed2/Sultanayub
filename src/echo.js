87cbf0083a9873aeb8536366d2fb070b823bd9dd
basantsaed2 <basantsaed718@gmail.com>
2026-06-16 14:52:05 +0300

    edits

--- src/echo.js
+++ src/echo.js
@@ -7,11 +7,10 @@ const echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    scheme: import.meta.env.VITE_REVERB_SCHEME ?? 'http',
-    forceTLS: false,
-    enabledTransports: ['ws'],
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    forceTLS: import.meta.env.VITE_REVERB_SCHEME === 'https',
+    enabledTransports: ['ws', 'wss'],
     disableStats: true,
 });
 
